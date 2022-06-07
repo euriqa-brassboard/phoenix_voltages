d5b2bcb765f3514c450797f33949f9a733e97df3
Yichao Yu <yyc1992@gmail.com>
2022-06-07 18:56:04 -0400

    [lib] shift pos to index again
    Zero in the center is a nice convention but it'll just add to the two conventions
    we already have to use (index and physical position) and make things more complicated.

--- lib/solution_process.jl
+++ lib/solution_process.jl
@@ -27,14 +27,12 @@ function Base.get(cache::FitCache{N}, idx::NTuple{N}) where N
     return res
 end
 
-# pos = 0 in the middle of the data range
+# pos is in index unit
 function _best_fit_idx(ntotal, order, pos)
     # for fit at `index`, the data covered is `index:(index + order)`
     # with the center at index `index + order / 2`.
-    # this corresponds to a position of `index + order / 2 - (ntotal + 1) / 2`
-    # or `index - (ntotal + 1 - order) / 2`
-    # Therefore, the ideal index to use for `pos` is `pos + (ntotal + 1 - order) / 2`
-    idx = round(Int, pos + (ntotal + 1 - order) / 2)
+    # Therefore, the ideal index to use for `pos` is `pos - order / 2`
+    idx = round(Int, pos - order / 2)
     if idx <= 1
         return 1
     elseif idx >= ntotal - order
@@ -49,13 +47,12 @@ function gradient(cache::FitCache{N}, dim, pos::Vararg{Any,N}) where N
     idxs = _best_fit_idx.(sizes, orders, pos)
     fit = get(cache, idxs)
     # center of the fit in index: idx + order / 2
-    # center of the fit in position: idx - (size + 1 - order) / 2
-    # position within fit: pos - idx + (size + 1 - order) / 2
-    pos = pos .- idxs .+ (sizes .+ 1 .- orders) ./ 2
+    # position within fit: pos - idx - order / 2
+    pos = pos .- idxs .- orders ./ 2
     return gradient(fit, dim, pos...)
 end
 
-function find_flat_point(data::A; init=ntuple(i->0.0, Val(N))) where (A<:AbstractArray{T,N} where T) where N
+function find_flat_point(data::A; init=ntuple(i->(size(data, i) + 1) / 2, Val(N))) where (A<:AbstractArray{T,N} where T) where N
     fitter = PolyFit.PolyFitter(ntuple(i->3, Val(N))...)
     cache = FitCache(fitter, data)
     function model!(g, x)
@@ -66,6 +63,18 @@ function find_flat_point(data::A; init=ntuple(i->0.0, Val(N))) where (A<:Abstrac
     end
     res = nlsolve(model!, collect(init))
     return ntuple(i->res.zero[i], Val(N))
+end
+
+function find_all_flat_points(all_data::A; init=ntuple(i->(size(all_data, i) + 1) / 2, Val(N - 1))) where (A<:AbstractArray{T,N} where T) where N
+
+    npoints = size(all_data, 3)
+    all_res = Matrix{Float64}(undef, npoints, N - 1)
+
+    for i in 1:npoints
+        init = find_flat_point(@view(all_data[:, :, i]), init=init)
+        all_res[i, :] .= init
+    end
+    return all_res
 end
 
 end
