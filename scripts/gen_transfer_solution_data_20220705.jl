a57226edc47fb4997ff4432db3b44487bd92694a
Yichao Yu <yyc1992@gmail.com>
2022-07-06 00:11:37 -0400

    Generate individual solutions

---
+++ scripts/gen_transfer_solution_data_20220705.jl
@@ -0,0 +1,27 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../lib"))
+
+import PhoenixVoltages.Optimizers
+using MAT
+
+const coeff_data, electrode_names = matopen(joinpath(@__DIR__, "../data/compensate_nozx_20220705.mat")) do mat
+    return read(mat, "data"), read(mat, "electrode_names")
+end
+
+const prefix = joinpath(@__DIR__, "../data/transfer_20220705")
+
+function solve_transfer(data)
+    @show data["xpos_um"]
+    solution = data["solution"]
+    x0 = solution[:, 7] .+ solution[:, 5] .* 0.75
+    vals = Optimizers.optimize_minmax_span(data["free_solution"], x0)
+    return Dict("electrodes"=>data["electrodes"], "voltages"=>vals,
+                "xpos_um"=>data["xpos_um"])
+end
+const transfer_solutions = [@time(solve_transfer(data)) for data in coeff_data]
+
+matopen("$(prefix).mat", "w") do mat
+    write(mat, "electrode_names", electrode_names)
+    write(mat, "transfer_solutions", transfer_solutions)
+end
