a1231ab8787cb72ac5bbc81d73969aa1c4f57666
Yichao Yu <yyc1992@gmail.com>
2022-07-09 10:36:50 -0400

    [lib] Remove optimizer model cache
    Doesn't seem to help performance for linear model

--- lib/PhoenixVoltages/src/optimizers.jl
+++ lib/PhoenixVoltages/src/optimizers.jl
@@ -4,56 +4,24 @@ import NLopt
 using JuMP
 using LinearAlgebra
 
-struct LinearConstraintMinMaxModel
-    model::Model
-    B::Matrix{VariableRef}
-    t::Vector{VariableRef}
-    x0::Vector{VariableRef}
-end
+function gen_minmax_model(B, x0)
+    nx, nt = size(B)
+    @assert nx == length(x0)
 
-function minmax_objective(x...)
-    return max(abs.(x)...)
-end
-
-function gen_minmax_model_template(nx, nt)
     model = Model(NLopt.Optimizer)
     set_optimizer_attribute(model, "algorithm", :LN_COBYLA)
-    @variable(model, x0[1:nx])
     @variable(model, t[1:nt])
-    @variable(model, B[1:nx, 1:nt])
     @variable(model, maxv)
     @constraint(model, maxv .>= B * t .+ x0)
     @constraint(model, maxv .>= .-(B * t .+ x0))
     @objective(model, Min, maxv)
-    return LinearConstraintMinMaxModel(model, B, t, x0)
-end
-
-const model_cache = Dict{Tuple{Int,Int},LinearConstraintMinMaxModel}()
-
-function gen_fixed_minmax_model(B, x0)
-    nx, nt = size(B)
-    @assert nx == length(x0)
-
-    model = pop!(model_cache, (nx, nt), nothing)
-    if model === nothing
-        model = gen_minmax_model_template(nx, nt)
-    end
-    for i in 1:length(B)
-        fix(model.B[i], B[i])
-    end
-    for i in 1:length(x0)
-        fix(model.x0[i], x0[i])
-    end
-    return model
+    return model, t
 end
 
 function optimize_minmax_span(B, x0)
-    model = gen_fixed_minmax_model(B, x0)
-    JuMP.optimize!(model.model)
-    t = Float64[value(t) for t in model.t]
-    nx, nt = size(B)
-    model_cache[(nx, nt)] = model
-    return B * t .+ x0
+    model, t = gen_minmax_model(B, x0)
+    JuMP.optimize!(model)
+    return B * value.(t) .+ x0
 end
 
 function optimize_minmax(A, y::AbstractVector)
@@ -68,12 +36,9 @@ function optimize_minmax(A, y::AbstractVector)
     # In another word, these are the x's that are orthogonal to all rows of A.
     # We can find the basis set that spans such space using QR decomposition.
     B = qr(A').Q[:, (ny + 1):nx]
-    model = gen_fixed_minmax_model(B, x0)
-    JuMP.optimize!(model.model)
-    t = Float64[value(t) for t in model.t]
-    nx, nt = size(B)
-    model_cache[(nx, nt)] = model
-    return B * t .+ x0
+    model, t = gen_minmax_model(B, x0)
+    JuMP.optimize!(model)
+    return B * value.(t) .+ x0
 end
 
 function optimize_minmax(A, y::AbstractMatrix)
