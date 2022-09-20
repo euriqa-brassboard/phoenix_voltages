dd11547a9f1f44d49d9d2624442d30206d08b7dd
Yichao Yu <yyc1992@gmail.com>
2022-09-20 15:47:35 -0400

    New shuttling solution

---
+++ scripts/gen_transfer_solution_data_smooth_20220920.jl
@@ -0,0 +1,80 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../lib"))
+
+import PhoenixVoltages.Optimizers
+using MAT
+import Ipopt
+using JuMP
+
+const coeff_data, electrode_names = matopen(joinpath(@__DIR__, "../data/compensate_nozx_20220920.mat")) do mat
+    return read(mat, "data"), read(mat, "electrode_names")
+end
+
+const prefix = joinpath(@__DIR__, "../data/transfer_smooth_20220920")
+
+function solve_all(diff_weight, flatten_max)
+    @show diff_weight
+    model = Model(Ipopt.Optimizer)
+    set_optimizer_attribute(model, "max_iter", 30000)
+    set_optimizer_attribute(model, "print_level", 5)
+    xs = Vector{AffExpr}[]
+    maxvs = VariableRef[]
+    maxdiffs = VariableRef[]
+    local prev_vmap
+    if flatten_max
+        @variable(model, maxmaxdiff)
+        maxmaxweight = 0
+    end
+    for data in coeff_data
+        solution = data["solution"]
+        x0 = solution[:, 7] .+ solution[:, 5] .* 0.75
+        B = data["free_solution"]
+        nx, nt = size(B)
+        @assert nx == length(x0)
+        t = @variable(model, [1:nt])
+        x = @expression(model, B * t .+ x0)
+        maxv = @variable(model)
+        @constraint(model, maxv .>= x)
+        @constraint(model, maxv .>= .-x)
+        push!(xs, x)
+        push!(maxvs, maxv)
+        vmap = Dict(zip(data["electrodes"], x))
+        if @isdefined(prev_vmap)
+            maxdiff = @variable(model)
+            for k in union(keys(vmap), keys(prev_vmap))
+                v1 = get(vmap, k, 0)
+                v2 = get(prev_vmap, k, 0)
+                @constraint(model, maxdiff >= v1 - v2)
+                @constraint(model, maxdiff >= v2 - v1)
+            end
+            push!(maxdiffs, maxdiff)
+            if flatten_max && -3050 < data["xpos_um"] < 1130
+                @constraint(model, maxmaxdiff >= maxdiff)
+                maxmaxweight += 1
+            end
+        end
+        prev_vmap = vmap
+    end
+    if flatten_max
+        @objective(model, Min, sum(maxvs) + sum(maxdiffs) * diff_weight + maxmaxdiff * maxmaxweight)
+    else
+        @objective(model, Min, sum(maxvs) + sum(maxdiffs) * diff_weight)
+    end
+    JuMP.optimize!(model)
+    return [[value(v) for v in x] for x in xs]
+end
+
+function pack_data(data, vals)
+    solution = data["solution"]
+    return Dict("electrodes"=>data["electrodes"], "voltages"=>vals,
+                "xpos_um"=>data["xpos_um"])
+end
+
+matopen("$(prefix).mat", "w") do mat
+    voltages = @time(solve_all(0.05, true))
+    transfer_solutions = [pack_data(data, vals) for (data, vals)
+                              in zip(coeff_data, voltages)]
+    write(mat, "electrode_names", electrode_names)
+    write(mat, "transfer_solutions", transfer_solutions)
+end
