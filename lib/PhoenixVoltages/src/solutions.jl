091c11fa6d9691562e4a80c13c2ebf7a7c668c14
Yichao Yu <yyc1992@gmail.com>
2022-09-20 13:56:45 -0400

    [lib] Fix expansion terms calculation scaling
    Hopefully this time I actually got the scaling correct...

--- lib/PhoenixVoltages/src/solutions.jl
+++ lib/PhoenixVoltages/src/solutions.jl
@@ -61,7 +61,7 @@ const l_unit_um = l_unit * 1e6 # um
 const V_unit_uV = V_unit * 1e6 # uV
 
 # Terms we care about
-# x, y, z, 2xy, 2yz, 2xz, z^2 - y^2, x^2 - (y^2 + z^2) / 2, x^3, x^4
+# x, y, z, xy, yz, xz, (z^2 - y^2) / 2, (x^2 - (y^2 + z^2) / 2) / 2, x^3 / 3!, x^4 / 4!
 # Since we care about the symmetry of the x^2 and z^2 term,
 # we actually do need to scale the x, y and z correctly.
 # stride should be in um, voltage should be in V
@@ -87,18 +87,19 @@ function get_compensate_terms1(res::Fitting.PolyFitResult{3}, stride)
     scaled_y = raw_y / stride[2]
     scaled_z = raw_z / stride[3]
 
-    # The actual term is 2xy, 2yz, 2zx in order to match the magnitude
-    # with the xx and zz terms.
-    scaled_xy = raw_xy / stride[1] / stride[2] / 2
-    scaled_yz = raw_yz / stride[2] / stride[3] / 2
-    scaled_zx = raw_zx / stride[3] / stride[1] / 2
+    # We need to divide the xy/yz/zx terms by 2 relative to the x2, y2, z2 terms.
+    # This makes sure that, e.g., the z^2 term is a direct rotation of the
+    # xy/yz/zx terms.
+    scaled_xy = raw_xy / stride[1] / stride[2]
+    scaled_yz = raw_yz / stride[2] / stride[3]
+    scaled_zx = raw_zx / stride[3] / stride[1]
 
-    scaled_x2 = raw_x2 / stride[1]^2
-    scaled_y2 = raw_y2 / stride[2]^2
-    scaled_z2 = raw_z2 / stride[3]^2
+    scaled_x2 = raw_x2 / stride[1]^2 * 2
+    scaled_y2 = raw_y2 / stride[2]^2 * 2
+    scaled_z2 = raw_z2 / stride[3]^2 * 2
 
-    scaled_x3 = raw_x3 / stride[1]^3
-    scaled_x4 = raw_x4 / stride[1]^4
+    scaled_x3 = raw_x3 / stride[1]^3 * 6
+    scaled_x4 = raw_x4 / stride[1]^4 * 24
 
     # The two legal quadratic terms are `x^2 - (y^2 + z^2) / 2` and `z^2 - y^2`
     # which are also orthogonal to each other.
@@ -161,7 +162,7 @@ function solve_compensate1(cache::Potentials.FitCache, pos::NTuple{3})
 end
 
 # Terms we care about during transport
-# x, y, z, 2xy, 2yz, z^2 - y^2, x^2 - (y^2 + z^2) / 2, x^3, x^4
+# x, y, z, xy, yz, (z^2 - y^2) / 2, (x^2 - (y^2 + z^2) / 2) / 2, x^3 / 3!, x^4 / 4!
 # zx is missing here since it seems to require a fairly high voltage to compensate
 # Since we care about the symmetry of the x^2 and z^2 term,
 # we actually do need to scale the x, y and z correctly.
@@ -187,17 +188,18 @@ function get_compensate_terms1_nozx(res::Fitting.PolyFitResult{3}, stride)
     scaled_y = raw_y / stride[2]
     scaled_z = raw_z / stride[3]
 
-    # The actual term is 2xy, 2yz in order to match the magnitude
-    # with the xx and zz terms.
-    scaled_xy = raw_xy / stride[1] / stride[2] / 2
-    scaled_yz = raw_yz / stride[2] / stride[3] / 2
+    # We need to divide the xy/yz/zx terms by 2 relative to the x2, y2, z2 terms.
+    # This makes sure that, e.g., the z^2 term is a direct rotation of the
+    # xy/yz/zx terms.
+    scaled_xy = raw_xy / stride[1] / stride[2]
+    scaled_yz = raw_yz / stride[2] / stride[3]
 
-    scaled_x2 = raw_x2 / stride[1]^2
-    scaled_y2 = raw_y2 / stride[2]^2
-    scaled_z2 = raw_z2 / stride[3]^2
+    scaled_x2 = raw_x2 / stride[1]^2 * 2
+    scaled_y2 = raw_y2 / stride[2]^2 * 2
+    scaled_z2 = raw_z2 / stride[3]^2 * 2
 
-    scaled_x3 = raw_x3 / stride[1]^3
-    scaled_x4 = raw_x4 / stride[1]^4
+    scaled_x3 = raw_x3 / stride[1]^3 * 6
+    scaled_x4 = raw_x4 / stride[1]^4 * 24
 
     # The two legal quadratic terms are `x^2 - (y^2 + z^2) / 2` and `z^2 - y^2`
     # which are also orthogonal to each other.
@@ -397,7 +399,8 @@ function compensation_to_file(solution::Potential, mapfile::MapFile,
 end
 
 # Terms we care about on pheonix
-# x, y, z, 2xy, 2yz, 2xz, z^2 - y^2, x^2 - (y^2 + z^2) / 2, x^3, x^4, x^2z
+# x, y, z, xy, yz, xz, (z^2 - y^2) / 2, (x^2 - (y^2 + z^2) / 2) / 2,
+# x^3 / 3!, x^4 / 4!, x^2z / 2
 # Compared to HOA, we are able to compensate for x^2z due to the outer electrodes.
 # stride should be in um, voltage should be in V
 function get_compensate_terms2(res::Fitting.PolyFitResult{3}, stride)
@@ -424,20 +427,21 @@ function get_compensate_terms2(res::Fitting.PolyFitResult{3}, stride)
     scaled_y = raw_y / stride[2]
     scaled_z = raw_z / stride[3]
 
-    # The actual term is 2xy, 2yz, 2zx in order to match the magnitude
-    # with the xx and zz terms.
-    scaled_xy = raw_xy / stride[1] / stride[2] / 2
-    scaled_yz = raw_yz / stride[2] / stride[3] / 2
-    scaled_zx = raw_zx / stride[3] / stride[1] / 2
+    # We need to divide the xy/yz/zx terms by 2 relative to the x2, y2, z2 terms.
+    # This makes sure that, e.g., the z^2 term is a direct rotation of the
+    # xy/yz/zx terms.
+    scaled_xy = raw_xy / stride[1] / stride[2]
+    scaled_yz = raw_yz / stride[2] / stride[3]
+    scaled_zx = raw_zx / stride[3] / stride[1]
 
-    scaled_x2 = raw_x2 / stride[1]^2
-    scaled_y2 = raw_y2 / stride[2]^2
-    scaled_z2 = raw_z2 / stride[3]^2
+    scaled_x2 = raw_x2 / stride[1]^2 * 2
+    scaled_y2 = raw_y2 / stride[2]^2 * 2
+    scaled_z2 = raw_z2 / stride[3]^2 * 2
 
-    scaled_x3 = raw_x3 / stride[1]^3
-    scaled_x4 = raw_x4 / stride[1]^4
+    scaled_x3 = raw_x3 / stride[1]^3 * 6
+    scaled_x4 = raw_x4 / stride[1]^4 * 24
 
-    scaled_x2z = raw_x2z / stride[1]^2 / stride[3]
+    scaled_x2z = raw_x2z / stride[1]^2 / stride[3] * 2
 
     # The two legal quadratic terms are `x^2 - (y^2 + z^2) / 2` and `z^2 - y^2`
     # which are also orthogonal to each other.
