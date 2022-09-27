358c45085b29313fcd9bb8c969f98eb7a64dea47
Yichao Yu <yyc1992@gmail.com>
2022-09-27 12:07:43 -0400

    Script to compute new transfer solution

---
+++ scripts/gen_transfer_solution_data_20220927.jl
@@ -0,0 +1,51 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../lib"))
+
+import PhoenixVoltages.Optimizers
+using MAT
+
+const coeff_data_nozx, electrode_names_nozx = matopen(joinpath(@__DIR__, "../data/compensate_nozx_20220927.mat")) do mat
+    return read(mat, "data"), read(mat, "electrode_names")
+end
+
+const coeff_data_zx, electrode_names_zx = matopen(joinpath(@__DIR__, "../data/compensate_20220927.mat")) do mat
+    return read(mat, "data"), read(mat, "electrode_names")
+end
+
+const xpos_ums = [data["xpos_um"] for data in coeff_data_zx]
+
+@assert size(coeff_data_zx) == size(coeff_data_nozx)
+@assert electrode_names_zx == electrode_names_nozx
+@assert [data["xpos_um"] for data in coeff_data_nozx] == xpos_ums
+
+const electrode_names = electrode_names_nozx
+
+const prefix = joinpath(@__DIR__, "../data/transfer_20220927")
+
+function solve_transfer(i)
+    xpos_um = xpos_ums[i]
+    @show xpos_um
+    # Stitch the zx vs no-zx solutions together at around -950 um,
+    # where the effect of relaxing the zx term should be minimal
+    # at least on the X2 anx YZ terms.
+    if xpos_um < -950
+        data = coeff_data_nozx[i]
+        solution = data["solution"]
+        x0 = solution[:, 7] .+ solution[:, 5] .* 0.75
+    else
+        data = coeff_data_zx[i]
+        solution = data["solution"]
+        x0 = solution[:, 8] .+ solution[:, 5] .* 0.75
+    end
+    @assert data["xpos_um"] == xpos_um
+    vals = Optimizers.optimize_minmax_span(data["free_solution"], x0)
+    return Dict("electrodes"=>data["electrodes"], "voltages"=>vals,
+                "xpos_um"=>xpos_um)
+end
+const transfer_solutions = [@time(solve_transfer(i)) for i in 1:length(xpos_ums)]
+
+matopen("$(prefix).mat", "w") do mat
+    write(mat, "electrode_names", electrode_names)
+    write(mat, "transfer_solutions", transfer_solutions)
+end
---
+++ scripts/gen_transfer_solution_data_smooth_20220927.jl
@@ -0,0 +1,146 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../lib"))
+
+import PhoenixVoltages.Optimizers
+using MAT
+import Ipopt
+using JuMP
+
+const coeff_data_nozx, electrode_names_nozx = matopen(joinpath(@__DIR__, "../data/compensate_nozx_20220927.mat")) do mat
+    return read(mat, "data"), read(mat, "electrode_names")
+end
+
+const coeff_data_zx, electrode_names_zx = matopen(joinpath(@__DIR__, "../data/compensate_20220927.mat")) do mat
+    return read(mat, "data"), read(mat, "electrode_names")
+end
+
+const xpos_ums = [data["xpos_um"] for data in coeff_data_zx]
+
+@assert size(coeff_data_zx) == size(coeff_data_nozx)
+@assert electrode_names_zx == electrode_names_nozx
+@assert [data["xpos_um"] for data in coeff_data_nozx] == xpos_ums
+
+const electrode_names = electrode_names_nozx
+
+const prefix = joinpath(@__DIR__, "../data/transfer_smooth_20220927")
+
+const coeff_data = Dict{String,Any}[]
+for i in 1:length(xpos_ums)
+    # Stitch the zx vs no-zx solutions together at around -950 um,
+    # where the effect of relaxing the zx term should be minimal
+    # at least on the X2 anx YZ terms.
+    # The global smoothing should hopefully make any transition in the crossover area
+    # smooth.
+    xpos_um = xpos_ums[i]
+    if xpos_um < -950
+        data = coeff_data_nozx[i]
+        solution = data["solution"]
+        x0 = solution[:, 7] .+ solution[:, 5] .* 0.75
+    else
+        data = coeff_data_zx[i]
+        solution = data["solution"]
+        x0 = solution[:, 8] .+ solution[:, 5] .* 0.75
+    end
+    @assert data["xpos_um"] == xpos_um
+    data["x0"] = x0
+    push!(coeff_data, data)
+end
+
+function solve_all(diff_weight, diff_flatten_weight, max_flatten_weight)
+    @show diff_weight
+    model = Model(Ipopt.Optimizer)
+    set_optimizer_attribute(model, "max_iter", 30000)
+    set_optimizer_attribute(model, "print_level", 5)
+    xs = Vector{AffExpr}[]
+    maxvs = VariableRef[]
+    maxdiffs = VariableRef[]
+    local prev_vmap
+    if diff_flatten_weight > 0
+        @variable(model, maxmaxdiff)
+        maxmaxdiffweight = 0
+    end
+    if max_flatten_weight > 0
+        @variable(model, maxmaxv)
+        maxmaxvweight = 0
+    end
+    for data in coeff_data
+        x0 = data["x0"]
+        B = data["free_solution"]
+        nx, nt = size(B)
+        @assert nx == length(x0)
+        t = @variable(model, [1:nt])
+        x = @expression(model, B * t .+ x0)
+        maxv = @variable(model)
+        @constraint(model, maxv .>= x)
+        @constraint(model, maxv .>= .-x)
+        push!(xs, x)
+        push!(maxvs, maxv)
+        vmap = Dict(zip(data["electrodes"], x))
+        if max_flatten_weight > 0 && -2500 < data["xpos_um"] < 1000
+            @constraint(model, maxmaxv >= maxv)
+            maxmaxvweight += 1
+        end
+        if @isdefined(prev_vmap)
+            maxdiff = @variable(model)
+            for k in union(keys(vmap), keys(prev_vmap))
+                v1 = get(vmap, k, 0)
+                v2 = get(prev_vmap, k, 0)
+                @constraint(model, maxdiff >= v1 - v2)
+                @constraint(model, maxdiff >= v2 - v1)
+            end
+            push!(maxdiffs, maxdiff)
+            if diff_flatten_weight > 0 && -3050 < data["xpos_um"] < 1000
+                @constraint(model, maxmaxdiff >= maxdiff)
+                maxmaxdiffweight += 1
+            end
+        end
+        prev_vmap = vmap
+    end
+    obj = sum(maxvs) + sum(maxdiffs) * diff_weight
+    if diff_flatten_weight > 0
+        obj = obj + maxmaxdiff * (maxmaxdiffweight * diff_flatten_weight)
+    end
+    if max_flatten_weight > 0
+        obj = obj + maxmaxv * (maxmaxvweight * max_flatten_weight)
+    end
+    @objective(model, Min, obj)
+    JuMP.optimize!(model)
+    return [[value(v) for v in x] for x in xs]
+end
+
+function pack_data(data, vals)
+    return Dict("electrodes"=>data["electrodes"], "voltages"=>vals,
+                "xpos_um"=>data["xpos_um"])
+end
+
+function gen_global()
+    voltages = @time(solve_all(0.05, 2, 10))
+    transfer_solutions = [pack_data(data, vals) for (data, vals)
+                              in zip(coeff_data, voltages)]
+    matopen("$(prefix).mat", "w") do mat
+        write(mat, "electrode_names", electrode_names)
+        write(mat, "transfer_solutions", transfer_solutions)
+    end
+end
+
+function gen_noglobal()
+    voltages = @time(solve_all(0.05, 0, 10))
+    transfer_solutions = [pack_data(data, vals) for (data, vals)
+                              in zip(coeff_data, voltages)]
+    matopen("$(prefix)_noglobal.mat", "w") do mat
+        write(mat, "electrode_names", electrode_names)
+        write(mat, "transfer_solutions", transfer_solutions)
+    end
+end
+
+if isempty(ARGS)
+    gen_global()
+    gen_noglobal()
+elseif ARGS[1] == "g"
+    gen_global()
+elseif ARGS[1] == "ng"
+    gen_noglobal()
+else
+    error("Invalid argument: $(ARGS)")
+end
