8f27c5891f759b4bcee13415f3a8cc9079fa8d9a
Yichao Yu <yyc1992@gmail.com>
2022-11-01 13:55:13 -0400

    Separate out model and builder

--- scripts/test_chain_pos_20221031.jl
+++ scripts/test_chain_pos_20221031.jl
@@ -3,28 +3,35 @@
 using JuMP
 using Interpolations
 
-mutable struct ModelBuilder{F,∇F,∇²F}
+struct IonChainModel{F,∇F,∇²F}
     model::Model
     f::F # Potential function
     ∇f::∇F
     ∇²f::∇²F
     ions::Vector{Pair{VariableRef,Float64}}
+    function IonChainModel{F,∇F,∇²F}(model::Model, f::F, ∇f::∇F, ∇²f::∇²F) where {F,∇F,∇²F}
+        register(model, :potential, 1, f, ∇f, ∇²f)
+        return new{F,∇F,∇²F}(model, f, ∇f, ∇²f, Pair{VariableRef,Float64}[])
+    end
+    function IonChainModel(model::Model, f::F, ∇f::∇F, ∇²f::∇²F) where {F,∇F,∇²F}
+        return IonChainModel{F,∇F,∇²F}(model, f, ∇f, ∇²f)
+    end
+end
+
+mutable struct ModelBuilder{F,∇F,∇²F}
+    model::IonChainModel{F,∇F,∇²F}
+    barrier_after_ion::Bool
     last_ion::VariableRef
     last_barrier::Float64
-    barrier_after_ion::Bool
-    function ModelBuilder{F,∇F,∇²F}(model::Model, f::F, ∇f::∇F, ∇²f::∇²F) where {F,∇F,∇²F}
-        builder = new{F,∇F,∇²F}(model, f, ∇f, ∇²f, Pair{VariableRef,Float64}[])
-        register(model, :potential, 1, f, ∇f, ∇²f)
-        builder.barrier_after_ion = false
-        return builder
+    function ModelBuilder{F,∇F,∇²F}(model::IonChainModel{F,∇F,∇²F}) where {F,∇F,∇²F}
+        return new{F,∇F,∇²F}(model, false)
     end
-    function ModelBuilder(model::Model, f::F, ∇f::∇F, ∇²f::∇²F) where {F,∇F,∇²F}
-        return ModelBuilder{F,∇F,∇²F}(model, f, ∇f, ∇²f)
+    function ModelBuilder(model::IonChainModel{F,∇F,∇²F}) where {F,∇F,∇²F}
+        return ModelBuilder{F,∇F,∇²F}(model)
     end
 end
 
 function add_barrier!(builder::ModelBuilder, barrier)
-    model = builder.model
     builder.last_barrier = barrier
     if !builder.barrier_after_ion
         if isdefined(builder, :last_ion)
@@ -37,14 +44,15 @@ end
 
 function add_ion!(builder::ModelBuilder, init_value, charge)
     model = builder.model
-    ion = @variable(model)
+    m = model.model
+    ion = @variable(m)
     set_start_value(ion, init_value)
-    push!(builder.ions, ion=>charge)
+    push!(model.ions, ion=>charge)
     if builder.barrier_after_ion
         set_lower_bound(ion, builder.last_barrier)
         builder.barrier_after_ion = false
     elseif isdefined(builder, :last_ion)
-        @constraint(model, builder.last_ion <= ion)
+        @constraint(m, builder.last_ion <= ion)
     end
     builder.last_ion = ion
     return ion
@@ -52,17 +60,19 @@ end
 
 function finalize_model!(builder::ModelBuilder)
     model = builder.model
-    obj = @NLexpression(model, 0)
-    nions = length(builder.ions)
-    for (i1, (ion1, charge1)) in enumerate(builder.ions)
-        obj = @NLexpression(model, obj + potential(ion1))
+    m = model.model
+    obj = @NLexpression(m, 0)
+    ions = model.ions
+    nions = length(ions)
+    for (i1, (ion1, charge1)) in enumerate(ions)
+        obj = @NLexpression(m, obj + potential(ion1))
         for i2 in (i1 + 1):nions
-            ion2, charge2 = builder.ions[i2]
-            obj = @NLexpression(model, obj + charge1 * charge2 / (ion2 - ion1))
+            ion2, charge2 = ions[i2]
+            obj = @NLexpression(m, obj + charge1 * charge2 / (ion2 - ion1))
         end
     end
-    @NLobjective(model, Min, obj)
-    return model
+    @NLobjective(m, Min, obj)
+    return
 end
 
 function interpolate_ref_functions(fi::Ref)
