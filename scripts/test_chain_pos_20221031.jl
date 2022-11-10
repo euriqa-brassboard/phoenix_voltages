446183214b8e1ea40e0af60785b0baca723742ef
Yichao Yu <yyc1992@gmail.com>
2022-11-09 19:18:52 -0500

    Playing around more with ion chain position/mode solver

--- scripts/test_chain_pos_20221031.jl
+++ scripts/test_chain_pos_20221031.jl
@@ -11,17 +11,18 @@ using PyPlot
 const model = Model(Ipopt.Optimizer)
 set_optimizer_attribute(model, "print_level", 0)
 
+const X = Ref(0.0)
 const X2 = Ref(0.0)
 const X4 = Ref(0.2)
 
-f(x) = (X2[] + X4[] * x^2) * x^2 + x * 2
-∇f(x) = (2 * X2[] + 4 * X4[] * x^2) * x + 2
+f(x) = (X2[] + X4[] * x^2) * x^2 + X[] * x
+∇f(x) = (2 * X2[] + 4 * X4[] * x^2) * x + X[]
 ∇²f(x) = 2 * X2[] + 12 * X4[] * x^2
 
 const chain_model = IonChainModel(model, f, ∇f, ∇²f)
 const builder = ModelBuilder(chain_model)
 
-const nions = 31
+const nions = 30
 
 for i in 1:nions
     add_ion!(builder, i - (nions - 1) / 2, 1)
@@ -30,6 +31,8 @@ end
 finalize_model!(builder)
 
 const ion_pos = [Float64[] for i in 1:nions]
+const axial_freqs = [Float64[] for i in 1:nions]
+const radial_freqs = [Float64[] for i in 1:nions]
 
 const x2s = range(0, -10, 1001)
 
@@ -39,12 +42,30 @@ const x2s = range(0, -10, 1001)
     for ((ion, charge), ions) in zip(chain_model.ions, ion_pos)
         push!(ions, value(ion))
     end
+    for (mode, freqs) in zip(axial_modes(chain_model), axial_freqs)
+        push!(freqs, mode)
+    end
+    for (mode, freqs) in zip(radial_modes(chain_model, x->5000), radial_freqs)
+        push!(freqs, mode)
+    end
     update_init_pos!(chain_model)
 end
 
 figure()
 for ions in ion_pos
     plot(x2s, ions)
+end
+grid()
+
+figure()
+for freqs in axial_freqs
+    plot(x2s, freqs)
+end
+grid()
+
+figure()
+for freqs in radial_freqs
+    plot(x2s, freqs)
 end
 grid()
 
--- scripts/test_chain_pos_lib_20221031.jl
+++ scripts/test_chain_pos_lib_20221031.jl
@@ -2,6 +2,7 @@
 
 using JuMP
 using Interpolations
+using LinearAlgebra
 
 struct IonChainModel{F,∇F,∇²F}
     model::Model
@@ -81,6 +82,55 @@ function update_init_pos!(model::IonChainModel)
     for ((ion, charge), v) in zip(model.ions, values)
         set_start_value(ion, v)
     end
+end
+
+function axial_modes(model::IonChainModel)
+    values = [value(ion) for (ion, charge) in model.ions]
+    nions = length(values)
+    H = zeros(nions, nions)
+    for i in 1:nions
+        pos = values[i]
+        H[i, i] = model.∇²f(pos)
+    end
+    for i2 in 2:nions
+        pos2 = values[i2]
+        for i1 in 1:i2 - 1
+            pos1 = values[i1]
+            term = 2 / (pos2 - pos1)^3
+            H[i1, i1] += term
+            H[i2, i2] += term
+            H[i1, i2] -= term
+            H[i2, i1] -= term
+        end
+    end
+    evs = eigvals(H)
+    return [ev >= 0 ? sqrt(ev) : -sqrt(-ev) for ev in evs]
+end
+
+# Input function computes the second order derivative of the RF potential
+# This assumes that the good axis for radial motion does not depend on
+# the ion position, otherwise we need to solve all radial modes at the same time...
+function radial_modes(model::IonChainModel, r_hess)
+    values = [value(ion) for (ion, charge) in model.ions]
+    nions = length(values)
+    H = zeros(nions, nions)
+    for i in 1:nions
+        pos = values[i]
+        H[i, i] = r_hess(pos)
+    end
+    for i2 in 2:nions
+        pos2 = values[i2]
+        for i1 in 1:i2 - 1
+            pos1 = values[i1]
+            term = 1 / (pos2 - pos1)^3
+            H[i1, i1] -= term
+            H[i2, i2] -= term
+            H[i1, i2] += term
+            H[i2, i1] += term
+        end
+    end
+    evs = eigvals(H)
+    return [ev >= 0 ? sqrt(ev) : -sqrt(-ev) for ev in evs]
 end
 
 function interpolate_ref_functions(fi::Ref)
