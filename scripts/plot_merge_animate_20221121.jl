859b5e8c61aa133ad720c8d001f68b75080ce343
Yichao Yu <yyc1992@gmail.com>
2022-11-23 10:44:43 -0500

    ... Wrong version of animated plot script

--- scripts/plot_merge_animate_20221121.jl
+++ scripts/plot_merge_animate_20221121.jl
@@ -19,62 +19,39 @@ end
 const transfer = load_solution(ARGS[2])
 const electrode_names = Vector{String}.(transfer.electrode_names)
 
-const coeff_data = matopen(ARGS[3]) do mat
-    return read(mat, "data")
-end
-
 const solution_file = ARGS[1]
 const solution = Potentials.import_pillbox_64(solution_file,
                                               electrode_names=electrode_names)
 const fits_cache = Potentials.FitCache(Fitting.PolyFitter(2, 2, 2), solution)
 const centers = Solutions.CenterTracker()
 
-function get_xpos_um(data)
-    xpos_um_min = [minimum(d["xpos_um"]) for d in data]
-    xpos_um_max = [maximum(d["xpos_um"]) for d in data]
-    xpos_min_diff = abs(xpos_um_min[1] - xpos_um_min[end])
-    xpos_max_diff = abs(xpos_um_max[1] - xpos_um_max[end])
-    return xpos_max_diff > xpos_min_diff ? xpos_um_max : xpos_um_min
-end
-const xpos_ums = get_xpos_um.(coeff_data)
+const trap_center_xidx = Potentials.x_axis_to_index(solution, 0)
+const trap_center_yidx = get(centers, trap_center_xidx)[1]
+const trap_center_ypos_um = Potentials.y_index_to_axis(solution,
+                                                       trap_center_yidx) * 1000
 
-const prefix = ARGS[4]
+const prefix = ARGS[3]
 
 const xrange_ums = -3220:1330
 
-function create_frame(title, sol, data)
-    xpos_ums = data["xpos_um"]
+function create_frame(data)
+    xpos_um = data["xpos_um"]
     frame = PhoenixVisual.render_frame(fits_cache, centers,
-                                       sol["electrodes"], sol["voltages"] ./ 5,
-                                       title=title,
-                                       xpos_um=xpos_ums[1], plotx_ums=xrange_ums,
+                                       data["electrodes"], data["voltages"] ./ 5,
+                                       title=@sprintf("% 5d μm", xpos_um),
+                                       xpos_um=xpos_um, plotx_ums=xrange_ums,
                                        finalize=false)
-    for i in 2:length(xpos_ums)
-        xpos_um = xpos_ums[i]
-        xidx = Potentials.x_axis_to_index(solution, xpos_um)
-        yidx = get(centers, xidx)[1]
-        ypos_um = Potentials.y_index_to_axis(solution, yidx) * 1000
-        c = PhoenixVisual.add_circle!(frame, xpos_um, ypos_um, 20)
-        c["fill"] = "blue"
-    end
+    c = PhoenixVisual.add_circle!(frame, 0, trap_center_ypos_um, 20)
+    c["fill"] = "blue"
     PhoenixVisual.finalize_svg!(frame)
     return frame
 end
 
-const xpos_prefixes = ["Single ion", "Chain", "Chain"]
-
-for si in 1:length(coeff_data)
-    solutions = transfer.transfer_solutions[si]
-    cd = coeff_data[si]
-    xpos_um = xpos_ums[si]
-    xpos_prefix = xpos_prefixes[si]
-    mkpath("$(prefix)/$si")
-    for (i, (solution, data)) in enumerate(zip(solutions, cd))
-        println("$i/$(length(cd))")
-        template = create_frame(@sprintf("%s: % 5d μm", xpos_prefix, xpos_um[i]),
-                                solution, data)
-        open("$(prefix)/$si/$i.svg", "w") do io
-            write(io, template)
-        end
+mkpath("$(prefix)")
+for (i, data) in enumerate(transfer.transfer_solutions)
+    @show i data["xpos_um"]
+    template = create_frame(data)
+    open("$(prefix)/$i.svg", "w") do io
+        write(io, template)
     end
 end
