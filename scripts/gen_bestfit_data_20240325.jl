c298fedb690a05a240eae61bf5047031407ccd45
Yichao Yu <yyc1992@gmail.com>
2024-04-03 10:35:34 -0400

    Hybrid generation

--- scripts/gen_bestfit_data_20240325.jl
+++ scripts/gen_bestfit_data_20240325.jl
@@ -94,58 +94,121 @@ function ElectrodeData(fit_cache, ele, index_range, center_index)
                           terms.z2, terms.x2, terms.x3, terms.x4])
 end
 
-const ele_data = [ElectrodeData(center_fit_cache, ele, index_range, center_index)
-                  for ele in ele_select]
+const slice_order_mapping = Dict((0, 0) => 1, (0, 1) => 2, (1, 0) => 3,
+                                 (0, 2) => 4, (1, 1) => 5, (2, 0) => 6)
 
-const order_mapping = Dict((0, 0) => 1, (0, 1) => 2, (1, 0) => 3,
-                           (0, 2) => 4, (1, 1) => 5, (2, 0) => 6)
-
-function generate_term(center_index, index_range, stride, order)
+function generate_term(center_index, index_range, stride_x_um, order)
     @assert all(0 .<= order)
     @assert order[3] + order[2] <= 2
-    param_idx = order_mapping[(order[3], order[2])]
+    param_idx = slice_order_mapping[(order[3], order[2])]
     len = length(index_range[1])
     res = zeros(6, len)
     xcenter = center_index[1]
-    xscale = (stride[1] * 1000) / Solutions.l_unit_um
+    xscale = stride_x_um / Solutions.l_unit_um
     coeff = 1 / factorial(order[1])
     for i in 1:len
         xpos = index_range[1][i] - xcenter
         res[param_idx, i] = (xpos * xscale)^order[1] * coeff
     end
-    return res
+    return vec(res)
 end
 
-const term_0 = generate_term(center_index, index_range, solution_stride,
-                             (0, 0, 0))
-const term_0_flat = vec(term_0)
-
-const coeff = Matrix{Float64}(undef, length(term_0_flat), length(ele_data) + 1)
-coeff[:, 1] .= term_0_flat
-for i in 1:length(ele_data)
-    coeff[:, 1 + i] .= ele_data[i].slice_data
+struct AllTermsData
+    ele_data::Vector{ElectrodeData}
+    center_x0::Matrix{Float64}
+    center_B::Matrix{Float64}
+    slice_coeff::Matrix{Float64}
+    slice_terms::Matrix{Float64}
 end
 
-function fit_term(model::Model, target, coeff, maxv, yz_weight)
-    nvars = size(coeff, 2)
-    @variable(model, -maxv .<= vars[1:nvars] .<= maxv)
-    # No limit on the DC term
-    delete_lower_bound(vars[1])
-    delete_upper_bound(vars[1])
-    err = coeff * vars .- vec(target)
+function AllTermsData(fit_cache, eles, index_range, center_index)
+    ele_data = [ElectrodeData(fit_cache, ele, index_range, center_index)
+                for ele in eles]
+
+    ncenter_coeff = length(ele_data[1].center_data)
+    center_coeff = Matrix{Float64}(undef, ncenter_coeff, length(ele_data))
+    for i in 1:length(ele_data)
+        center_coeff[:, i] .= ele_data[i].center_data
+    end
+    center_x0 = center_coeff \ Matrix(I, ncenter_coeff, ncenter_coeff)
+    center_B = qr(center_coeff').Q[:, (ncenter_coeff + 1):end]
+
+    stride_x_um = solution.stride[3] * 1000
+    term_0 = generate_term(center_index, index_range, stride_x_um, (0, 0, 0))
+    slice_coeff = Matrix{Float64}(undef, length(term_0), length(ele_data) + 1)
+    slice_coeff[:, 1] .= term_0
+    for i in 1:length(ele_data)
+        slice_coeff[:, 1 + i] .= ele_data[i].slice_data
+    end
+    term_x1_raw = generate_term(center_index, index_range, stride_x_um, (1, 0, 0))
+    term_x2_raw = generate_term(center_index, index_range, stride_x_um, (2, 0, 0))
+    term_x3_raw = generate_term(center_index, index_range, stride_x_um, (3, 0, 0))
+    term_x4_raw = generate_term(center_index, index_range, stride_x_um, (4, 0, 0))
+
+    term_y1_raw = generate_term(center_index, index_range, stride_x_um, (0, 1, 0))
+    term_y2_raw = generate_term(center_index, index_range, stride_x_um, (0, 2, 0))
+
+    term_z1_raw = generate_term(center_index, index_range, stride_x_um, (0, 0, 1))
+    term_z2_raw = generate_term(center_index, index_range, stride_x_um, (0, 0, 2))
+
+    term_x1y1_raw = generate_term(center_index, index_range, stride_x_um, (1, 1, 0))
+    term_y1z1_raw = generate_term(center_index, index_range, stride_x_um, (0, 1, 1))
+    term_x1z1_raw = generate_term(center_index, index_range, stride_x_um, (1, 0, 1))
+
+    term_x1y2_raw = generate_term(center_index, index_range, stride_x_um, (1, 2, 0))
+    term_x2y2_raw = generate_term(center_index, index_range, stride_x_um, (2, 2, 0))
+    term_x1z2_raw = generate_term(center_index, index_range, stride_x_um, (1, 0, 2))
+    term_x2z2_raw = generate_term(center_index, index_range, stride_x_um, (2, 0, 2))
+
+    term_x1 = term_x1_raw
+    term_y1 = term_y1_raw
+    term_z1 = term_z1_raw
+
+    term_xy = term_x1y1_raw
+    term_yz = term_y1z1_raw
+    term_zx = term_x1z1_raw
+    term_z2 = term_z2_raw - term_y2_raw
+    term_x2 = term_x2_raw .- (term_y2_raw .+ term_z2_raw) ./ 2
+
+    term_x3 = term_x3_raw .- (term_x1y2_raw .+ term_x1z2_raw) ./ 2
+    term_x4 = term_x4_raw .- (term_x2y2_raw .+ term_x2z2_raw) ./ 2
+
+    slice_terms = hcat(term_x1, term_y1, term_z1,
+                       term_xy, term_yz, term_zx, term_z2, term_x2,
+                       term_x3, term_x4)
+    return AllTermsData(ele_data, center_x0, center_B, slice_coeff, slice_terms)
+end
+
+function fit_term(model::Model, term_data::AllTermsData, term_idx, maxv, yz_weight)
+    x0 = @view(term_data.center_x0[:, term_idx])
+    B = term_data.center_B
+    nx, nt = size(B)
+    @show nx, nt
+    @variable(model, t[1:nt])
+    x = @expression(model, B * t .+ x0)
+    @constraint(model, -maxv .<= x .<= maxv)
+
+    nvars = nx + 1
+    @variable(model, dc_offset)
+    vars = [dc_offset; x]
+    target = @view(term_data.slice_terms[:, term_idx])
+    @show size(term_data.slice_coeff)
+    @show size(vars)
+    @show size(term_data.slice_terms)
+    err = term_data.slice_coeff * vars .- target
     nvals = length(err)
     @variable(model, abserr[1:nvals])
     @constraint(model, .-abserr .<= err)
     @constraint(model, abserr .>= err)
-    abserr = reshape(abserr, size(target))
+    npoints = length(target) ÷ 6
+    abserr = reshape(abserr, (6, npoints))
 
     @variable(model, maxerr)
 
-    npoints = size(target, 2)
     point_center = (1 + npoints) / 2
     max_offset = point_center - 1
     C = log(2) / max_offset^2
-    for i in 1:size(target, 2)
+    for i in 1:npoints
         x_weight = exp(-(i - point_center)^2 * C)
         for j in 1:6
             e = abserr[j, i] * x_weight * yz_weight[j]
@@ -158,31 +221,14 @@ function fit_term(model::Model, target, coeff, maxv, yz_weight)
     return value.(vars)
 end
 
-const term_x1 = generate_term(center_index, index_range, solution_stride,
-                              (1, 0, 0))
-const term_x2_raw = generate_term(center_index, index_range, solution_stride,
-                                  (2, 0, 0))
-const term_y2_raw = generate_term(center_index, index_range, solution_stride,
-                                  (0, 2, 0))
-const term_z2_raw = generate_term(center_index, index_range, solution_stride,
-                                  (0, 0, 2))
+const all_term_data =
+    AllTermsData(center_fit_cache, ele_select, index_range, center_index)
 
-const term_x2 = term_x2_raw .- (term_y2_raw .+ term_z2_raw) ./ 2
-const term_zz = term_y2_raw .- term_z2_raw
-
-const term_x1_flat = vec(term_x1)
-const term_x2_flat = vec(term_x2)
-const term_zz_flat = vec(term_zz)
-
-const voltages_x1 = coeff \ term_x1_flat
-const voltages_x2 = coeff \ term_x2_flat
-const voltages_zz = coeff \ term_zz_flat
-
-const voltages_x1_2 = fit_term(Model(Ipopt.Optimizer), term_x1, coeff, 0.3,
+const voltages_x1_2 = fit_term(Model(Ipopt.Optimizer), all_term_data, 1, 0.3,
                                (4, 3, 3, 1, 3, 1))
-const voltages_x2_2 = fit_term(Model(Ipopt.Optimizer), term_x2, coeff, 10.0,
+const voltages_zz_2 = fit_term(Model(Ipopt.Optimizer), all_term_data, 7, 10.0,
                                (40, 3, 3, 2000, 300, 2000))
-const voltages_zz_2 = fit_term(Model(Ipopt.Optimizer), term_zz, coeff, 10.0,
+const voltages_x2_2 = fit_term(Model(Ipopt.Optimizer), all_term_data, 8, 10.0,
                                (40, 3, 3, 2000, 300, 2000))
 
 function get_nth_part(data, idx)
@@ -191,63 +237,62 @@ end
 
 const term_names = ["1", "y", "z", "y2", "yz", "z2"]
 
-@show extrema(voltages_x1[2:end])
-true_x1 = coeff * voltages_x1
-err_x1 = term_x1_flat .- true_x1
-@show extrema(err_x1)
+# @show extrema(voltages_x1[2:end])
+# true_x1 = coeff * voltages_x1
+# err_x1 = term_x1_flat .- true_x1
+# @show extrema(err_x1)
 
 @show extrema(voltages_x1_2[2:end])
-true_x1_2 = coeff * voltages_x1_2
+term_x1_flat = all_term_data.slice_terms[:, 1]
+true_x1_2 = all_term_data.slice_coeff * voltages_x1_2
 err_x1_2 = term_x1_flat .- true_x1_2
 @show extrema(err_x1_2)
 
-@show extrema(voltages_x2[2:end])
-true_x2 = coeff * voltages_x2
-err_x2 = term_x2_flat .- true_x2
-@show extrema(err_x2)
+# @show extrema(voltages_x2[2:end])
+# true_x2 = coeff * voltages_x2
+# err_x2 = term_x2_flat .- true_x2
+# @show extrema(err_x2)
 
 @show extrema(voltages_x2_2[2:end])
-true_x2_2 = coeff * voltages_x2_2
+term_x2_flat = all_term_data.slice_terms[:, 8]
+true_x2_2 = all_term_data.slice_coeff * voltages_x2_2
 err_x2_2 = term_x2_flat .- true_x2_2
 @show extrema(err_x2_2)
 
-@show extrema(voltages_zz[2:end])
-true_zz = coeff * voltages_zz
-err_zz = term_zz_flat .- true_zz
-@show extrema(err_zz)
+# @show extrema(voltages_zz[2:end])
+# true_zz = coeff * voltages_zz
+# err_zz = term_zz_flat .- true_zz
+# @show extrema(err_zz)
 
-@show extrema(voltages_zz_2[2:end])
-true_zz_2 = coeff * voltages_zz_2
-err_zz_2 = term_zz_flat .- true_zz_2
-@show extrema(err_zz_2)
-
+# @show extrema(voltages_zz_2[2:end])
+# true_zz_2 = coeff * voltages_zz_2
+# err_zz_2 = term_zz_flat .- true_zz_2
+# @show extrema(err_zz_2)
 
 # for i in 1:6
 #     figure()
 #     plot(get_nth_part(term_x1_flat, i), label="X1 tgt")
-#     plot(get_nth_part(true_x1, i), label="X1")
 #     plot(get_nth_part(true_x1_2, i), label="X1_2")
-#     legend()
-#     title(term_names[i])
-# end
-
-# for i in 1:6
-#     figure()
-#     plot(get_nth_part(term_x2_flat, i), label="X2 tgt")
-#     plot(get_nth_part(true_x2, i), label="X2")
-#     plot(get_nth_part(true_x2_2, i), label="X2_2")
 #     legend()
 #     title(term_names[i])
 # end
 
 for i in 1:6
     figure()
-    plot(get_nth_part(term_zz_flat, i), label="ZZ tgt")
-    plot(get_nth_part(true_zz, i), label="ZZ")
-    plot(get_nth_part(true_zz_2, i), label="ZZ_2")
+    plot(get_nth_part(term_x2_flat, i), label="X2 tgt")
+    plot(get_nth_part(true_x2_2, i), label="X2_2")
     legend()
     title(term_names[i])
 end
+
+# for i in 1:6
+#     figure()
+#     plot(get_nth_part(term_zz_flat, i), label="ZZ tgt")
+#     plot(get_nth_part(true_zz, i), label="ZZ")
+#     plot(get_nth_part(true_zz_2, i), label="ZZ_2")
+#     legend()
+#     title(term_names[i])
+# end
 
 # for i in 1:6
 #     figure()
